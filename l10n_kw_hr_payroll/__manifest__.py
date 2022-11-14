732d31d4fb4ee9cdf61c4a3e39880be2ae04db4f
redapureskill <mi_reda2012@live.fr>
2022-11-14 10:14:55 +0000

    [UPD] Fl10n_kuw_payroll for odoo apps

--- l10n_kw_hr_payroll/__manifest__.py
+++ l10n_kw_hr_payroll/__manifest__.py
@@ -1,11 +1,17 @@
 # coding: utf-8 
 {
     'name': 'Kuwait - Payroll',
-    'author': 'Alkhuzam & Co',
+    'version': '15.0.0.1',
+    "author": "Alkhuzam & Co. - Morison Advisory",
+    'summary': 'Kuwait Payroll and End of Service rules.',
     'category': 'Human Resources/Payroll',
     'description': """
 Kuwait Payroll and End of Service rules.
 ========================================
+Configuration of hr_payroll for kuwait localization
+Calculating the basic salary for the employees following the kuwait law.
+Calculating the end of service and provision
+Daily computation of leaves and end of service for each contracted employee.
     """,
     'depends': ['hr_payroll'],
     'data': [
@@ -16,7 +22,13 @@ Kuwait Payroll and End of Service rules.
         'views/hr_employee_public_views.xml',
         'report/hr_contract_history_report_views.xml'
     ],
-    'license': 'OEEL-1',
+    'license': 'OPL-1',
+    "installable": True,
     'auto_install': False,
     'application':True,
+    'price': 100,
+    'currency': 'USD',
+    'images': ['static/description/banner.gif'],
+    'company': 'Alkhuzam & Co.- Morison Advisory',
+    'maintainer': 'Alkhuzam & Co.- Morison Advisory',
 }
---
+++ l10n_kw_hr_payroll/readme.txt
@@ -0,0 +1,6 @@
+
+Version => 15.0.0.1 
+
+#Collaborators
+1- MOHAMMED RIDA YAHLA -> Odoo developer at Alkhuzam - Morison advisory
+
